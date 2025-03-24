38fa21b563baf89f9555a7c8190ea4381899e2ad
gadlely <dorlsmd123@gmail.com>
2025-03-24 10:00:21 +0900

    2025-03-24 초기설정

---
+++ src/main/java/Offime/Offime/OffimeApplication.java
@@ -0,0 +1,13 @@
+package Offime.Offime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffimeApplication.class, args);
+	}
+
+}
