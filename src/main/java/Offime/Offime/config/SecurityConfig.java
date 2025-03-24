6a94bc43a6b686a23d8c2342dba31deb428895c0
sumin <rallarura92@naver.com>
2025-03-24 18:35:47 +0900

    feat: 2025-03-24 Member 생성

--- src/main/java/Offime/Offime/config/SecurityConfig.java
+++ src/main/java/Offime/Offime/config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package Offime.Offime.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable());
+        http.formLogin((formLogin) -> formLogin.loginPage("/login"));
+        http.authorizeHttpRequests((authorize) ->
+                authorize.requestMatchers("/**").permitAll()
+        );
+        return http.build();
+    }
 }
--- src/main/java/Offime/Offime/service/member/MemberService.java
+++ src/main/java/Offime/Offime/service/member/MemberService.java
@@ -1,4 +1,33 @@
 package Offime.Offime.service.member;
 
+import Offime.Offime.dto.request.member.MemberRegisterDto;
+import Offime.Offime.dto.response.member.MemberResponseDto;
+import Offime.Offime.entity.member.Member;
+import Offime.Offime.repository.member.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    public MemberResponseDto register(MemberRegisterDto memberRegisterDto){
+        String encodedPassword = passwordEncoder.encode(memberRegisterDto.getPassword());
+        memberRegisterDto.setPassword(encodedPassword);
+        isExistUserEmail(memberRegisterDto.getEmail());
+        Member saveMember = memberRepository.save(MemberRegisterDto.ofEntity(memberRegisterDto));
+        return MemberResponseDto.fromEntity(saveMember);
+    }
+
+    private void isExistUserEmail(String email) {
+        if (memberRepository.findByEmail(email).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+    }
 }
