81bece2a5d47f2b3d49669f85be74f23f4f9cb31
SIJIN9090 <dol9090@naver.com>
2025-03-24 10:19:07 +0900

    2025-03-24 초기설정

---
+++ src/test/java/Offime/Offime/OffimeApplicationTests.java
@@ -0,0 +1,13 @@
+package Offime.Offime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
