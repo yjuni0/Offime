7397ffc6110013840bc59683bdfcb28749cda904
sumin <rallarura92@naver.com>
2025-03-25 18:42:07 +0900

    update: 2025-03-25

--- src/main/java/Offime/Offime/security/jwt/JwtTokenUtil.java
+++ src/main/java/Offime/Offime/security/jwt/JwtTokenUtil.java
@@ -23,26 +23,21 @@ public class JwtTokenUtil implements Serializable {
     @Value("${jwt.tokenExpirationTime}") private Integer tokenExpirationTime;
     @Value("${jwt.secret}") private String secret;
 
-    // 블랙리스트(무효화된 토큰 저장)
     private final Set<String> invalidatedTokens = ConcurrentHashMap.newKeySet();
 
-    // extract username from jwt token
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
 
-    // extract expiration date from jwt token
     public Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
     }
 
-    //
     public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
 
-    // extract any information from token with secret key
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parserBuilder()  // parserBuilder()로 변경
                 .setSigningKey(secret)
@@ -51,23 +46,16 @@ public class JwtTokenUtil implements Serializable {
                 .getBody();
     }
 
-    // check token expired
     private Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
 
-    //generate token for user
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return doGenerateToken(claims, userDetails.getUsername());
     }
 
-    //while creating the token -
-    //1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
-    //2. Sign the JWT using the HS512 algorithm and secret key.
-    //3. According to JWS Compact Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
-    //   compaction of the JWT to a URL-safe string
     private String doGenerateToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + tokenExpirationTime * 1000))
@@ -89,5 +77,4 @@ public class JwtTokenUtil implements Serializable {
     public boolean isTokenInvalidated(String token) {
         return invalidatedTokens.contains(token);
     }
-
 }
