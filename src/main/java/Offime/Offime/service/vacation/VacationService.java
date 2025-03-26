225eb6d998d2d13a51b5cadd54a461d25cf8735a
yjuni0 <alstn9654@naver.com>
2025-03-26 13:19:34 +0900

    엔티티 변환 메서드 가지고 있는 빈등록 클래스 생성

--- src/main/java/Offime/Offime/service/vacation/VacationService.java
+++ src/main/java/Offime/Offime/service/vacation/VacationService.java
@@ -26,12 +26,13 @@ import java.util.List;
 @Transactional
 public class VacationService {
     private final VacationRepository vacationRepository;
+    private final VacationMapper vacationMapper;
     private final MemberRepository memberRepository;
     // 조회
     public Page<ResVacation> getAllVacations(Member member, Pageable pageable) {
         log.info("요청 멤버{} 페이징{} : ", member.getId(),pageable);
         Page<Vacation> vacations = vacationRepository.findAllByMember(member,pageable);
-        List<ResVacation> list = vacations.stream().map(ResVacation::fromEntity).toList();
+        List<ResVacation> list = vacations.stream().map(vacationMapper::fromEntity).toList();
         log.info("해당 멤버 {} 가 신청한 휴가 {}",member.getId(),list.size());
 
         return new PageImpl<>(list,pageable,list.size());
@@ -45,16 +46,16 @@ public class VacationService {
             throw new IllegalArgumentException("해당 멤버만 조회 가능 "+member.getId());
         }
         log.info("요청 휴가 {} 멤버 id {} ",id,member.getId());
-        return ResVacation.fromEntity(vacation);
+        return vacationMapper.fromEntity(vacation);
     }
 
     // 신청
     public ResVacation applyVacation(Member member, ReqVacation reqVacation) {
         log.info("휴가 신청 멤버 id {} 요청 데이터 : {} ", member.getId(),reqVacation);
-        Vacation vacation = ReqVacation.toEntity(member, reqVacation);
+        Vacation vacation = vacationMapper.toEntity(member, reqVacation);
         vacationRepository.save(vacation);
         log.info("휴가 신청 성공 멤버{} , 휴가 id{} ",member.getId(),vacation.getId());
-        return ResVacation.fromEntity(vacation);
+        return vacationMapper.fromEntity(vacation);
     }
 
     // 승인 반려
