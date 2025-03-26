35da37d25a5f6f6810e8cec21cbccf4592a39c52
yjuni0 <alstn9654@naver.com>
2025-03-26 13:17:53 +0900

    DTO 명확성 수정 Entity 변환 메서드 삭제

--- src/main/java/Offime/Offime/dto/request/vacation/ReqVacation.java
+++ src/main/java/Offime/Offime/dto/request/vacation/ReqVacation.java
@@ -9,15 +9,4 @@ import java.time.LocalDate;
 
 public record ReqVacation(String type, String startDate, String endDate, String reason) {
 
-
-    public static Vacation toEntity(Member member, ReqVacation reqVacation) {
-        return Vacation.builder()
-                .member(member)
-                .type(VacationType.fromDescription(reqVacation.type))
-                .startDate(LocalDate.parse(reqVacation.startDate()))
-                .endDate(LocalDate.parse(reqVacation.endDate()))
-                .reason(reqVacation.reason)
-                .status(VacationApprovalStatus.WAITING)
-                .build();
-    }
 }
