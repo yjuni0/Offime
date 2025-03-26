5b2fc39f0c7ed146801feb5bf4ad62f63955d582
yjuni0 <alstn9654@naver.com>
2025-03-26 18:10:55 +0900

    개발 단계 전체 인가 설정

--- src/main/java/Offime/Offime/config/SecurityConfig.java
+++ src/main/java/Offime/Offime/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package Offime.Offime.config;
 
+import Offime.Offime.security.jwt.JwtAuthenticationEntryPoint;
+import Offime.Offime.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +13,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CorsConfigurationSource corsConfigurationSource;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     // 비밀번호 암호화 - Bean 설정
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -33,10 +40,23 @@ public class SecurityConfig {
     // HttpSecurity 보안 설정,  Jwt 인증을 사용할 때 사용
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable());
-        http.sessionManagement((session) -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
-        return http.build();
+        return http
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+
+                .authorizeHttpRequests(authorize
+                        -> authorize
+                        .requestMatchers(
+                                "*"
+                        ).permitAll()
+                        .anyRequest().authenticated()  // 나머지 모든 요청은 인증 필요
+                )
+
+
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(excep -> excep.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
