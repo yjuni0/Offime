61a234f9706f7c18b584cd9228aa018308fa6ffa
yjuni0 <alstn9654@naver.com>
2025-03-27 18:46:03 +0900

    메시지 브로커 publish,consumer 설정

---
+++ src/main/java/Offime/Offime/config/rabbitMQ/MessagePublisher.java
@@ -0,0 +1,32 @@
+package Offime.Offime.config.rabbitMQ;
+
+
+import Offime.Offime.entity.notifications.NotificationMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Configuration;
+
+
+@RequiredArgsConstructor
+@Configuration
+public class MessagePublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    // 휴가 메시지 발행
+    public void sendVacationMessage(String routingKey, NotificationMessage notificationMessage) {
+        rabbitTemplate.convertAndSend(RabbitConfig.VACATION_EXCHANGE, routingKey, notificationMessage);
+    }
+    public void sendVacationApprovedMessage(String routingKey, NotificationMessage notificationMessage) {
+        rabbitTemplate.convertAndSend(RabbitConfig.VACATION_EXCHANGE, routingKey, notificationMessage);
+    }
+    public void sendVacationRejectedMessage(String routingKey, NotificationMessage notificationMessage) {
+        rabbitTemplate.convertAndSend(RabbitConfig.VACATION_EXCHANGE, routingKey, notificationMessage);
+    }
+
+    // 비용 메시지 발행
+    public void sendCostMessage(String routingKey, String message) {
+        rabbitTemplate.convertAndSend(RabbitConfig.COST_EXCHANGE, routingKey, message);
+    }
+
+}
