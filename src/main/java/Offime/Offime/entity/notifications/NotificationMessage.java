f50bc8a1161f081fb885f630fc6d7f8078baff73
yjuni0 <alstn9654@naver.com>
2025-03-27 18:45:35 +0900

    메시지 브로커 제이슨 직렬화 및 역직렬화를 위한 빈등록,레빗엠큐 설정

---
+++ src/main/java/Offime/Offime/entity/notifications/NotificationMessage.java
@@ -0,0 +1,16 @@
+package Offime.Offime.entity.notifications;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationMessage {
+    private Long memberId;
+    private String message;
+}
