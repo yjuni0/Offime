608758583876033c53d4950f59c43643fe4e5d17
yjuni0 <alstn9654@naver.com>
2025-03-28 18:04:10 +0900

    알림 메시지 필드 및 로직 구현

--- src/main/java/Offime/Offime/config/rabbitMQ/NotificationService.java
+++ src/main/java/Offime/Offime/config/rabbitMQ/NotificationService.java
@@ -1,34 +1,25 @@
 package Offime.Offime.config.rabbitMQ;
 
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import Offime.Offime.entity.member.Member;
+import Offime.Offime.entity.notifications.NotificationMessage;
+import Offime.Offime.repository.notification.NotificationMessageRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
+
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class NotificationService {
 
-    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final NotificationMessageRepository notificationMessageRepository;
 
-    // 클라이언트 연결을 위한 메서드
-    public SseEmitter listenToNotifications(Long memberId) {
-        SseEmitter emitter = new SseEmitter();
-        emitters.put(memberId, emitter);
-        emitter.onCompletion(() -> emitters.remove(memberId));  // 연결 종료 시 처리
-        emitter.onTimeout(() -> emitters.remove(memberId));      // 타임아웃 시 처리
-        return emitter;
+    public List<NotificationMessage> getAll(Member member) {
+        Long memberId = member.getId();
+        List<NotificationMessage> list = notificationMessageRepository.findAllByMemberId(memberId);
+        return list;
     }
 
-    // 알림을 클라이언트에게 전송하는 메서드
-    public void sendNotification(Long memberId, String notification) {
-        SseEmitter emitter = emitters.get(memberId);
-        if (emitter != null) {
-            try {
-                emitter.send(notification);  // 클라이언트에게 알림 전송
-            } catch (IOException e) {
-                emitter.completeWithError(e);  // 전송 중 오류 발생 시
-            }
-        }
-    }
 }
