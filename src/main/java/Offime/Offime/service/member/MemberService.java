ad3b93f3bada0268d740888fb32caf3ab7218a2a
sumin <rallarura92@naver.com>
2025-03-28 18:29:36 +0900

    fix: member 코드 수정

--- src/main/java/Offime/Offime/service/member/MemberService.java
+++ src/main/java/Offime/Offime/service/member/MemberService.java
@@ -2,11 +2,14 @@ package Offime.Offime.service.member;
 
 import Offime.Offime.dto.request.member.MemberLoginDto;
 import Offime.Offime.dto.request.member.MemberRegisterDto;
+import Offime.Offime.dto.response.member.MemberListDto;
 import Offime.Offime.dto.response.member.MemberResponseDto;
 import Offime.Offime.dto.response.member.MemberTokenDto;
 import Offime.Offime.entity.member.Member;
+import Offime.Offime.entity.member.SignUpStatus;
 import Offime.Offime.exception.MemberException;
 import Offime.Offime.exception.ResourceNotFoundException;
+import Offime.Offime.exception.UnauthorizedAccessException;
 import Offime.Offime.repository.member.MemberRepository;
 import Offime.Offime.security.jwt.JwtTokenUtil;
 import jakarta.transaction.Transactional;
@@ -18,7 +21,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -34,13 +36,16 @@ public class MemberService {
     private final JwtTokenUtil jwtTokenUtil;
     private final AuthenticationManager authenticationManager;
 
-
     // 회원가입
-    public MemberResponseDto register(MemberRegisterDto memberRegisterDto){
+    public MemberResponseDto register(MemberRegisterDto memberRegisterDto) {
         String encodedPassword = passwordEncoder.encode(memberRegisterDto.getPassword());
         memberRegisterDto.setPassword(encodedPassword);
         isExistUserEmail(memberRegisterDto.getEmail());
-        Member saveMember = memberRepository.save(MemberRegisterDto.ofEntity(memberRegisterDto));
+        Member saveMember = MemberRegisterDto.ofEntity(memberRegisterDto);
+        if (saveMember.getSignUpStatus() == null) {
+            saveMember.setSignUpStatus(SignUpStatus.PENDING);
+        }
+        saveMember = memberRepository.save(saveMember);
         return MemberResponseDto.fromEntity(saveMember);
     }
 
@@ -55,6 +60,15 @@ public class MemberService {
         Member member = memberRepository.findByEmail(memberLoginDto.getEmail()).orElseThrow(
                 () -> new ResourceNotFoundException("Member", "Member Email", memberLoginDto.getEmail()));
         authenticate(memberLoginDto.getEmail(), memberLoginDto.getPassword());
+
+        if (member.getSignUpStatus() == SignUpStatus.PENDING) {
+            throw new UnauthorizedAccessException("가입 승인 대기중입니다.");
+        }
+
+        if (!member.isEnable()) {
+            throw new DisabledException("비활성화 처리된 계정입니다.");
+        }
+
         String password = memberLoginDto.getPassword();
         if (passwordEncoder.matches(password, member.getPassword())) {
             String token = jwtTokenUtil.generateToken(member);
@@ -62,6 +76,7 @@ public class MemberService {
         } else {
             throw new IllegalArgumentException("잘못된 비밀번호 입니다.");
         }
+
     }
 
     private void authenticate(String email, String password) {
@@ -75,7 +90,25 @@ public class MemberService {
     }
 
     // 모든 직원 조회
-    public Page<MemberResponseDto> getAllMembers(Pageable pageable) {
-        return memberRepository.findAll(pageable).map(MemberResponseDto::fromEntity);
+    public Page<MemberListDto> getAllMembers(Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberListDto::fromEntity);
+    }
+
+    // 탈퇴
+    public void userDisabled(Member member) {
+        member.setEnable(false);
+        memberRepository.save(member);
+    }
+
+    // ADMIND -> USER 삭제
+    public void deleteMember(Long id) {
+            memberRepository.deleteById(id);
+    }
+
+    // 직원 상세페이지
+    public MemberResponseDto getMemberDetail(Long id) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("직원 정보를 찾을 수 없습니다."));
+        return MemberResponseDto.fromEntity(member);
     }
 }
