dd2cfdf4547f654d414a7e49c2062893a03c949d
SIJIN9090 <dol9090@naver.com>
2025-03-28 10:41:27 +0900

    2025-03-28 업데이트

--- src/main/front/src/component/router/Router.js
+++ src/main/front/src/component/router/Router.js
@@ -1,6 +1,6 @@
 import Home from "../Home";
 import ForCheck from "../reports/ForCheck";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import TemplateCreate from "../reports/templates/TemplateCreate";
 import ReportCreateTemplateSelect from "../reports/reports/create/ReportCreateTemplateSelect";
 import ReportCreate from "../reports/reports/create/ReportCreate";
@@ -11,22 +11,24 @@ import TemplateList from "../reports/templates/TemplateList";
 import ReportUpdate from "../reports/reports/update/ReportUpdate";
 import Schedule from "../schedule/Schedule";
 
-
 function Router() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/templates/create" element={<TemplateCreate />} />
-            <Route path="/reports/templateList" element={<ReportCreateTemplateSelect/>}/>
-            <Route path="/reports/create/:templateId" element={<ReportCreate/>}/>
-            <Route path="/reports/read/:reportId" element={<ReportRead/>}/>
-            <Route path="/reports/read" element={<ReportList/>}/>
-            <Route path="/replies/:reportId" element={<ReportReplies/>}/>
-            <Route path="/schedule" element={<Schedule />} />
-            <Route path="/templates/list" element={<TemplateList/>} />
-            <Route path="/reports/update/:reportId" element={<ReportUpdate/>}/>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/templates/create" element={<TemplateCreate />} />
+      <Route
+        path="/reports/templateList"
+        element={<ReportCreateTemplateSelect />}
+      />
+      <Route path="/reports/create/:templateId" element={<ReportCreate />} />
+      <Route path="/reports/read/:reportId" element={<ReportRead />} />
+      <Route path="/reports/read" element={<ReportList />} />
+      <Route path="/replies/:reportId" element={<ReportReplies />} />
+      <Route path="/schedule" element={<Schedule />} />
+      <Route path="/templates/list" element={<TemplateList />} />
+      <Route path="/reports/update/:reportId" element={<ReportUpdate />} />
+    </Routes>
+  );
 }
 
 export default Router;
--- src/main/java/Offime/Offime/config/rabbitMQ/MessageConsumer.java
+++ src/main/java/Offime/Offime/config/rabbitMQ/MessageConsumer.java
@@ -1,36 +1,36 @@
-package Offime.Offime.config.rabbitMQ;
-
-
-import Offime.Offime.entity.notifications.NotificationMessage;
-import Offime.Offime.repository.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.context.annotation.Configuration;
-
-
-
-@Slf4j
-@Configuration
-@RequiredArgsConstructor
-public class MessageConsumer {
-    private final NotificationService notificationService; // NotificationService 주입
-
-    // 휴가 관련 메시지 수신
-    @RabbitListener(queues = RabbitConfig.VACATION_QUEUE)
-    public void handleVacationMessage(NotificationMessage notificationMessage) {
-        Long memberId = notificationMessage.getMemberId();
-        String message = notificationMessage.getMessage();
-        // 회원에게 알림 전송
-        notificationService.sendNotification(memberId, message);
-
-        log.info("휴가 관련 메시지 : {}",  message);
-    }
-
-    // 비용 관련 메시지 수신
-    @RabbitListener(queues = RabbitConfig.COST_QUEUE)
-    public void handleCostMessage(String message) {
-        log.info("비용 관련 메시지 : {}",  message);
-    }
-
-}
+//package Offime.Offime.config.rabbitMQ;
+//
+//
+//import Offime.Offime.entity.notifications.NotificationMessage;
+//import Offime.Offime.repository.member.MemberRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//
+//@Slf4j
+//@Configuration
+//@RequiredArgsConstructor
+//public class MessageConsumer {
+//    private final NotificationService notificationService; // NotificationService 주입
+//
+//    // 휴가 관련 메시지 수신
+//    @RabbitListener(queues = RabbitConfig.VACATION_QUEUE)
+//    public void handleVacationMessage(NotificationMessage notificationMessage) {
+//        Long memberId = notificationMessage.getMemberId();
+//        String message = notificationMessage.getMessage();
+//        // 회원에게 알림 전송
+//        notificationService.sendNotification(memberId, message);
+//
+//        log.info("휴가 관련 메시지 : {}",  message);
+//    }
+//
+//    // 비용 관련 메시지 수신
+//    @RabbitListener(queues = RabbitConfig.COST_QUEUE)
+//    public void handleCostMessage(String message) {
+//        log.info("비용 관련 메시지 : {}",  message);
+//    }
+//
+//}
