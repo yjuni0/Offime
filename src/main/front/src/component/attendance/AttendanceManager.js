ddf82dc6d2e234ad4dbc1df823eb618723f60ae3
dewey827 <dldudgus827@gmail.com>
2025-03-31 09:50:13 +0900

    03-31 1교시

---
+++ src/main/front/src/component/attendance/AttendanceManager.js
@@ -0,0 +1,10 @@
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+function AttendanceManager() {
+    
+    // return (
+
+    // );
+}
+export default AttendanceManager;
