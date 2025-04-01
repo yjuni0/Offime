58cdc34cbc28b5dd4e4cc8660dcbac69056006ac
seongwoo <ksoung140w@gmail.com>
2025-04-01 16:18:19 +0900

    update: 캘린더뷰 UI 완료_20250401

--- src/main/front/src/component/schedule/Calender.js
+++ src/main/front/src/component/schedule/Calender.js
@@ -5,11 +5,16 @@ function Calender(){
     const today = new Date();
     const [currentDate, setCurrentDate] = useState(new Date());
     const [activeDay, setActiveDay] = useState(null);
-    const [selectedDate, setSelectedDate] = useState("");
+    const defaultDate = (today.getFullYear() + "-" + (today.getMonth() +1) + "-" + today.getDate()).toString() ;
+    const [selectedDate, setSelectedDate] = useState(defaultDate);
+    const dayNames = ['일', '월', '화', '수', '목', '금', '토'];
+    const [selectedDay, setSelectedDay] = useState(dayNames[today.getDay()]);
     const navigate = useNavigate();
 
     const [selectedYear, setSelectedYear] = useState();
     const [selectedMonth, setSelectedMonth] = useState();
+
+    const [editOpen, setEditOpen] = useState(false);
 
     useEffect(() => {
         renderCalendar();
@@ -28,10 +33,12 @@ function Calender(){
 
         // 지난달 날짜 추가 (비활성)
         for (let i = firstDay - 1; i >= 0; i--) {
+            const date = new Date(year, month -1 , prevLastDate - i);
             daysArray.push({
                 year,
                 month: month - 1,
                 day: prevLastDate - i,
+                dayName : dayNames[date.getDay()],
                 inactive: true,
             });
         }
@@ -39,12 +46,13 @@ function Calender(){
 
         // 이번 달 날짜 추가
         for (let i = 1; i <= lastDate; i++) {
+            const date = new Date(year,month, i);
             const isToday = year === today.getFullYear() && month === today.getMonth() && i === today.getDate();
-
             daysArray.push({
                 year,
                 month,
                 day: i,
+                dayName: dayNames[date.getDay()],
                 today: isToday,
                 inactive: false, // 오늘 이전 날짜는 비활성
             });
@@ -52,10 +60,12 @@ function Calender(){
 
         // 다음 달 날짜 추가
         for (let i = 1; i < 7 - lastDay; i++) {
+            const date = new Date(year,month + 1, i);
             daysArray.push({
                 year,
                 month: month + 1,
                 day: i,
+                dayName: dayNames[date.getDay()],
                 inactive: true,
             });
         }
@@ -66,19 +76,26 @@ function Calender(){
     }
 
     function handleSelectDay(dayObj) {
+        console.log(dayObj);
+        console.log(dayObj.dayName)
         setActiveDay(dayObj);
         setSelectedDate(
             `${dayObj.year}-${String(dayObj.month + 1).padStart(2, "0")}-${String(dayObj.day).padStart(2, "0")}`
         );
+        setSelectedDay(`${dayObj.dayName}`);
     }
+
+    function handleEdit() {
+        setEditOpen(prev => !prev);
+    }
+
 
     return(
         <>
-            <div className="calender-container">
-                <p className="hidden">calender page</p>
+            <div className= {editOpen ? "calendar-container active" : "calendar-container"}>
+                <p className="hidden">calendar page</p>
                 <div className="calender-wrap">
                     <div className="calendar">
-                        <input type="text" value={selectedDate} readOnly id="selectedDate" placeholder="선택 날짜 확인용"/>
                         <div className="calendar-header">
                             <p id="monthYear" className="cal-month">
                                 {currentDate.getFullYear()}년 {currentDate.getMonth() + 1}월
@@ -103,6 +120,14 @@ function Calender(){
                                         onClick={() => handleSelectDay(date)}
                                     >
                                         <p>{date.day}</p>
+                                        <div className="sched red">
+                                            <p className="fs_xsm active">사전 미팅aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                                            <p className="fs_xsm">10:00 ~ 13:00</p>
+                                        </div>
+                                        <div className="sched green">
+                                            <p className="fs_xsm active">사전 미팅aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                                            <p className="fs_xsm">10:00 ~ 13:00</p>
+                                        </div>
                                     </div>
                                 );
                             })}
@@ -110,9 +135,29 @@ function Calender(){
                     </div>
                 </div>
             </div>
-            <div className="dark-area"></div>
-            <div className="schedule-detail">
-
+            {/*<div className={editOpen ? "dark-area active" : "dark-area"} onClick={handleEdit}></div>*/}
+            <div className={editOpen ? "schedule-detail active" : "schedule-detail"}>
+                    <div className="sched-date">
+                        <input type="text" value={selectedDate + ` ` + `(` + selectedDay + `)`} readOnly id="selectedDate"/>
+                    </div>
+                <div className="inner">
+                    <p className="fs_md mt_lg">일정 시간</p>
+                    <div className='item-flex mt_md'>
+                        <input type="time" className='input-time' id="start-time"/> <p className="fs_md">~</p> <input type="time" className='input-time' id="end-time"/>
+                    </div>
+                    <p className="fs_md mt_lg mb_md">휴식 시간</p>
+                    <input type="time" className='input-time' id="break-time"/>
+                    <p className="fs_md mt_lg">메모</p>
+                    <textarea className='textarea mt_md fs_md' rows={5}></textarea>
+                    <button className="btn btn-max btn-pm fs_lg mt_lg">스케줄 등록</button>
+                </div>
+            </div>
+            <div className="sched-btn" onClick={handleEdit}>
+                {editOpen ? (
+                  <img src="/image/icon_close.svg" alt="스케줄 편집 닫기"/>
+                ) : (
+                  <img src="/image/icon_write.svg" alt="스케줄 편집 열기"/>
+                )}
             </div>
         </>
     );
