a92a3972c61293d4cc377e0bdcae3d3bb8d1bb8a
OYEs <160278000+yjuni0@users.noreply.github.com>
2025-04-02 00:42:23 +0000

    파일 충돌 삭제

--- src/main/front/src/main/java/Offime/Offime/repository/reports/TemplateAccessRepository.java
+++
@@ -1,9 +0,0 @@
-package Offime.Offime.repository.reports;
-
-import Offime.Offime.entity.reports.TemplateAccess;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TemplateAccessRepository extends JpaRepository<TemplateAccess, Long> {
-}
--- src/main/front/src/main/java/Offime/Offime/repository/reports/TemplatesRepository.java
+++
@@ -1,9 +0,0 @@
-package Offime.Offime.repository.reports;
-
-import Offime.Offime.entity.reports.Templates;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TemplatesRepository extends JpaRepository<Templates, Long> {
-}
