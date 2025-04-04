524a21b0c4aad7f801a50f7e0f5e894d41853a30
sumin <rallarura92@naver.com>
2025-04-04 10:09:35 +0900

    docs: 주석 제거

--- src/main/front/src/component/member/button/LeaveBtn.js
+++ src/main/front/src/component/member/button/LeaveBtn.js
@@ -3,11 +3,10 @@ import useLeaveBtn from '../api/useLeaveBtn';
 
 function LeaveBtn() {
     const { handleLeave, message } = useLeaveBtn();
-    const userRole = localStorage.getItem('role'); // 로그인한 사용자의 권한 정보 가져오기
+    const userRole = localStorage.getItem('role');
 
     return (
         <div>
-            {/* ADMIN일 경우 '회원 삭제' 버튼, USER일 경우 '회원 탈퇴' 버튼 */}
             <button className="Logout-btn" onClick={handleLeave}>
                 {userRole === 'ADMIN' ? '회원 삭제' : '회원 탈퇴'}
             </button>
