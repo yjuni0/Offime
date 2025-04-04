ad17c12c3fd0a143dadbe0c59b48bed265591ec8
sumin <rallarura92@naver.com>
2025-04-04 19:43:00 +0900

    fix: 디자인 추가

--- src/main/front/src/component/member/pages/SignUpConfirm.js
+++ src/main/front/src/component/member/pages/SignUpConfirm.js
@@ -5,34 +5,53 @@ import useSignUpConfirm from '../api/useSignUpConfirm';
 const SignUpConfirm = () => {
     const { pendingMembers, error, updateSignUpStatusToActive } =
         useSignUpConfirm();
+
     return (
         <section className="sec">
             <div className="inner">
                 <div className="item">
                     <p className="fs_lg">가입 승인</p>
-
-                    <hr />
+                    <br />
                     {error && <div>{error}</div>}
                     <table>
                         <tbody>
                             {pendingMembers.length === 0 ? (
                                 <tr>
-                                    <td colSpan="2">가입 신청자가 없습니다.</td>
+                                    <td colSpan="2" className="no-data">
+                                        가입 신청자가 없습니다.
+                                    </td>
                                 </tr>
                             ) : (
                                 pendingMembers.map((member) => (
                                     <tr key={member.email}>
                                         <td
-                                            className="btn btn-pm-f btn-max"
+                                            className="member_bar"
                                             onClick={() =>
                                                 updateSignUpStatusToActive(
                                                     member.email
                                                 )
                                             }
                                         >
-                                            {member.name}
-                                            <br />
-                                            {member.email}
+                                            <div className="member_info">
+                                                <div className="profile_picture">
+                                                    <img
+                                                        src={
+                                                            member.profileImageUrl
+                                                                ? `${process.env.PUBLIC_URL}${member.profileImageUrl}`
+                                                                : '/image/member/profile_no_image.jpg'
+                                                        }
+                                                        alt="프로필"
+                                                    />
+                                                </div>
+                                                <div className="member_details">
+                                                    <span className="member_name">
+                                                        {member.name}
+                                                    </span>
+                                                    <span className="member_team">
+                                                        {member.email}
+                                                    </span>
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
                                 ))
